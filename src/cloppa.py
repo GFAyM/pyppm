d603ee5bfa124fdcd3ca0f14bd4a82a9012b0f29
bajac <danielbajac94@gmail.com>
2022-04-04 18:02:02 -0300

    sigue faltando agregar las matrices de Fock localizadas a la matriz general,  se propone agregar los elementos a la manera del Pyscf

--- src/cloppa.py
+++ src/cloppa.py
@@ -1,4 +1,3 @@
-from tabnanny import verbose
 from pyscf import gto, scf
 from pyscf.gto import Mole
 import numpy as np
@@ -26,7 +25,6 @@ class Cloppa:
     def __attrs_post_init__(self):
         self.mol = gto.M(atom=str(self.mol_input), basis=self.basis, verbose=0)
         self.mf = scf.RHF(self.mol).run()
-        #fock_canonical = self.mf.get_fock()
 
     @property
     def fock_matrix_canonical(self):
@@ -102,6 +100,106 @@ class Cloppa:
 
         return M
 
+
+
+@attr.s
+class Cloppa_reduced_occupied:
+    """
+    Cloppa method for the calculation of NMR J-coupling using localized molecular orbitals of any kind
+    e.g Foster-Boys, Pipek-Mezey, etc. 
+    This method follows the Giribet et. al article: Molecular Physics 91: 1, 105-112
+    
+    This class pretends calculate only the contribution of a pair of occupied orbitals 
+    and all virtual orbitals
+    """
+    
+    mol_input = attr.ib(default=None, type=str, validator=attr.validators.instance_of(str))
+    basis = attr.ib(default='cc-pvdz', type=str, validator=attr.validators.instance_of(str))
+    mo_coeff_loc = attr.ib(default=None, type=np.array)
+    mol_loc = attr.ib(default=None)
+    o1 = attr.ib(default=None, type=list)
+    o2 = attr.ib(default=None, type=list)
+    virt = attr.ib(default=None, type=list)
+    
+    def __attrs_post_init__(self):
+        self.mol = gto.M(atom=str(self.mol_input), basis=self.basis, verbose=0)
+        self.mf = scf.RHF(self.mol).run()
+
+    @property
+    def fock_matrix_canonical(self):
+        self.fock_canonical = self.mf.get_fock()
+        return self.fock_canonical
+
+    @property
+    def inverse_prop_pol(self):
+        self.i = self.mo_coeff_loc[:, self.o1]
+        self.j = self.mo_coeff_loc[:, self.o2]
+
+        self.virt = self.mo_coeff_loc[:,self.virt]
+        self.fock_matrix_canonical
+        block11 = np.zeros((len(self.virt),len(self.virt)))
+        #now, we sum the F_{ab}\delta_{ab} localized Fock matrix 
+        block11 += self.virt.T @ self.fock_canonical @ self.virt
+
+        # this is the F_{ij}\delta_{ab} localized Fock matrix
+        F_ii = self.i.T @ self.fock_canonical @ self.i
+        np.fill_diagonal(block11, block11.diagonal() - F_ii)
+
+        block11 -= ao2mo.general(self.mol_loc, 
+                [self.virt,self.virt,self.i,self.i],
+                compact=False).reshape(len(self.virt),len(self.virt))    
+        
+        block11 -= ao2mo.general(self.mol_loc, 
+                [self.virt,self.i,self.virt,self.i],
+                compact=False).reshape(len(self.virt),len(self.virt))    
+
+
+        block12 = np.zeros((len(self.virt),len(self.virt)))
+        #now, the block12 matrix only contain the F_{ij} element in all the diagonal
+        F_ij = self.i.T @ self.fock_canonical @ self.j
+        np.fill_diagonal(block12, -F_ij)
+        block12 -= ao2mo.general(self.mol_loc, [self.virt,self.virt,self.j,self.i],
+         compact=False).reshape(len(self.virt),len(self.virt))
+        block12 -= ao2mo.general(self.mol_loc, 
+                [self.virt,self.j,self.virt,self.i],
+                compact=False).reshape(len(self.virt),len(self.virt))    
+
+
+        block21 = np.zeros((len(self.virt),len(self.virt)))
+        #now, the block21 matrix only contain the F_{ij} element in all the diagonal
+        F_ji = self.j.T @ self.fock_canonical @ self.i
+        np.fill_diagonal(block21, -F_ji)
+        block21 -= ao2mo.general(self.mol_loc, [self.virt,self.virt,self.i,self.j],
+            compact=False).reshape(len(self.virt),len(self.virt))
+        block21 -= ao2mo.general(self.mol_loc, 
+                [self.virt,self.i,self.virt,self.j],
+                compact=False).reshape(len(self.virt),len(self.virt))    
+
+
+        block22 = np.zeros((len(self.v1 + self.v2),len(self.v1 + self.v2)))
+        block22 += self.virt.T @ self.fock_canonical @ self.virt
+        F_jj = self.j.T @ self.fock_canonical @ self.j
+        np.fill_diagonal(block22, block22.diagonal() - F_jj) 
+        block22 -= ao2mo.general(self.mol_loc,
+                [self.virt,self.virt,self.j,self.j],
+                compact=False).reshape(len(self.virt),len(self.virt))
+        block22 -= ao2mo.general(self.mol_loc, 
+                [self.virt,self.j,self.virt,self.j],
+                compact=False).reshape(len(self.virt),len(self.virt))    
+        
+        
+        superblock1 = np.concatenate((block11, block12), axis=1)
+        superblock2 = np.concatenate((block21, block22), axis=1)
+        M = np.concatenate((superblock1, superblock2), axis=0)
+        #M_diag = np.zeros(M.shape)
+        #np.fill_diagonal(M_diag, np.diag(M))
+
+
+        return M
+
+
+
+
 @attr.s
 class Cloppa_test:
     """
@@ -118,8 +216,6 @@ class Cloppa_test:
     o2 = attr.ib(default=None, type=list)
     v1 = attr.ib(default=None, type=list)
     v2 = attr.ib(default=None, type=list)
-
-
 
     @property
     def inverse_prop_pol(self):
